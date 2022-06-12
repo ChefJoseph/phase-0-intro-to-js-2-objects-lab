891d73b1e43d5ef0cb5cd2786544b9caf04b9e06
Joseph Hirotsu <jhirotsu@gmail.com>
2022-06-12 13:40:25 -0400

    commit tree d47ecf7a2fc9954ba183cc12ed6252cfbc3544ce

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name: "Joe",
+    streetAddress: "33 Bond Street",
+};
+
+function updateEmployeeWithKeyAndValue(employee,key,value) {
+    const employees = {...employee}
+    employees[key] = value
+    return employees;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) {
+    employee[key] = value
+    return employee;
+}
+function deleteFromEmployeeByKey(employee,key,value) {
+    const newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key,value) {
+    delete employee[key]
+    return employee;
+}
